5f1001eb1eb461db5e95c6fab0616041dda89547
AbdElrhman Mohamed <abdelrhman132006@gmail.com>
2026-06-16 02:18:26 +0300

    Update DFS-Iterative.cpp

--- GraphAlgorithms/DFS-Iterative.cpp
+++ GraphAlgorithms/DFS-Iterative.cpp
@@ -2,19 +2,19 @@ void dfs(vector<vector<int>>& adj, int u) {
     vector<bool> vis(adj.size(), false);
     stack<int> st;
     st.push(u);
+    vis[u] = true;
 
     while (!st.empty()) {
         int curr = st.top();
         st.pop();
 
-        if (vis[curr]) continue;
-        vis[curr] = true;
         cout << curr << ' ';
 
-        for (int i = adj[curr].size() - 1; i >= 0; --i) {
+        for (int i = (int)adj[curr].size() - 1; i >= 0; --i) {
             int child = adj[curr][i];
             if (!vis[child]) {
                 st.push(child);
+                vis[child] = true;
             }
         }
     }
