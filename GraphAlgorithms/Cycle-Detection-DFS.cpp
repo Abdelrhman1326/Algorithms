b2e7f34d311609a5f3ecb1da354b02e38d14462a
Abdelrhman132006 <abdelrhman132006@gmail.com>
2026-06-19 19:06:34 +0300

    DfS With Cycle Detection

---
+++ GraphAlgorithms/Cycle-Detection-DFS.cpp
@@ -0,0 +1,21 @@
+vector<vector<int>> adj;
+vector<int> visited; // 0 = unvisited, 1 = visiting, 2 = fully visited
+vector<int> order;
+bool has_cycle = false;
+
+void dfs(int u) {
+    if (has_cycle) return;
+    visited[u] = 1;
+
+    for (int v : adj[u]) {
+        if (visited[v] == 1) {
+            has_cycle = true;
+            return;
+        } else if (visited[v] == 0) {
+            dfs(v);
+        }
+    }
+
+    visited[u] = 2;
+    order.push_back(u);
+}
